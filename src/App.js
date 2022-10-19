f1417e7f4136950ee0a95a29f3ec46f20ccf2be1
Rushenka Jayasuriya <rushenka.jayasuriya@student.unsw.edu.au>
2022-10-19 23:37:42 +1100

    Changed text to cehck AWS re-deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in 7 days
+          Learn React in less than a day
         </a>
       </header>
     </div>
